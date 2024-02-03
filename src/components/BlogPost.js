20d64948fd9cbaaf150db9bd805b44844918124f
Alex Partida <alexgpartida@gmail.com>
2024-02-02 21:48:38 -0800

    add usewindowsize and use it to size images for mobile and desktop

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import useWindowSize from './useWindowSize'
+
 
 const BlogPost = ({ title, text, imgs, imgAlts }) => {
+
+    const windowSize = useWindowSize();
 
     return <div className='blog-post'>
         <h3 className='blog-title'>{title} </h3>
         <p className='blog-text'>{text}</p>
         <div className='img-container'>
-            {imgs.map((img, i) => <img src={img} alt={imgAlts[i]} width="300" height="auto" className='blog-image'/>)}
+            {imgs.map((img, i) => <img src={img} alt={imgAlts[i]} width={windowSize.width < 600 ? "200" : "300"} height="auto" className='blog-image' />)}
         </div>
     </div>
 }
