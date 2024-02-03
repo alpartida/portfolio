f2cd64446f9039ecac29dae80d335d84cd6cf8a3
Alex Partida <alexgpartida@gmail.com>
2024-02-02 19:57:30 -0800

    remove work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Homepage from './pages/homepage/Homepage';
 import About from './pages/about/About';
-import Work from './pages/work/Work';
 import Blog from './pages/blog/Blog';
 import {
   BrowserRouter,
@@ -13,7 +12,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path='/work' element={<Work />} />
         <Route path='/about' element={<About />} />
         <Route path='/blog' element={<Blog />} />
       </Routes>
