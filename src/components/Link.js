8b5a2b071e57551fab6d984154c3de208a3fa3a4
Alex Partida <alexgpartida@gmail.com>
2024-02-03 17:04:30 -0800

    added animation to the big text on homepage and work page

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
 const NavLink = (props) => {
+
+    const [skew, setSkew] = useState(0);
+    const [skewString, setSkewString] = useState(`skew(0deg)`);
+
+    useEffect(() => {
+        setSkewString(`skew(${skew}deg)`);
+    }, [skew]);
+
     return (
-        <Link onClick={() => props.updateAnimate(false)} to={props.link}>
-            <h2 className="nav-item">
+        <Link onClick={() => props.updateAnimate(false)} to={props.text}>
+            <h2 className="nav-item" style={{ transform: skewString }} onMouseEnter={() => setSkew(-10)} onMouseLeave={() => setSkew(0)} onTouchStart={() => setSkew(-10)} onTouchEnd={() => setSkew(0)}>
                 {props.text}
             </h2>
-        </Link>
+        </Link >
     )
 }
 
--- src/components/WorkLink.js
+++ src/components/WorkLink.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const WorkLink = (props) => {
+    const [skew, setSkew] = useState(0);
+    const [skewString, setSkewString] = useState(`skew(0deg)`);
+
+    useEffect(() => {
+        setSkewString(`skew(${skew}deg)`);
+    }, [skew]);
+
+
     return (
-        <a href={props.link} className="work-link" target="_blank" rel="noreferrer">{props.text}</a>)
+        <a href={props.link} className="work-link" target="_blank" rel="noreferrer" style={{ transform: skewString }} onMouseEnter={() => setSkew(-10)} onMouseLeave={() => setSkew(0)} onTouchStart={() => setSkew(-10)} onTouchEnd={() => setSkew(0)}>{props.text}</a>)
 }
 
 export default WorkLink;
